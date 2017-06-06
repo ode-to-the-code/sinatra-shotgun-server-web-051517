eb68c6ead62dd4e9a040aa17ed75f6449d807b0a
ode-to-the-code <justcallmethis@gmail.com>
2017-06-06 09:27:10 -0400

    done

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! i built this!!! it is my masterpiece"
   end
 
 end
